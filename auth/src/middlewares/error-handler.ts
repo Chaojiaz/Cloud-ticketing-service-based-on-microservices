52af95a0b1423a3fd1924cb89870b1ff387aad47
Chaojiaz <chaojiaz@andrew.cmu.edu>
2022-07-11 20:43:38 -0400

    add custom-error abstract class to simplfy and unify customize error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -8,24 +7,8 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-
-    const formattedErrors = err.errors.map(error => {
-      return { message: error.msg, field: error.param };
-    });
-
-    console.log('handling this error as a request validation error');
-
-    return res.status(400).send({ errors: formattedErrors });
-
-  }
-  if (err instanceof DatabaseConnectionError) {
-    console.log('handling this error as a databese connection error');
-    return res.status(500).send({
-      errors: [
-        { message: err.reson }
-      ]
-    });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
