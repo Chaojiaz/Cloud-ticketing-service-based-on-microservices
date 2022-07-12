7e4522127ba2887c33b4f96682fb78bd98111a3e
Chaojiaz <chaojiaz@andrew.cmu.edu>
2022-07-12 17:22:22 -0400

    MongoDB instance up and running, and authtification application connected to it.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose, { Mongoose } from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -16,12 +18,25 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', () => {
+app.all('*', async (req, res) => {
   throw new NotFoundError()
 });
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connecting to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  // if pass the try/catch successfully, it is time to listen to traffic.
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!');
+  });
+};
+
+start();
+
