c021836923bda90a07b2b7df20cec41a3bdadb89
Chaojiaz <chaojiaz@andrew.cmu.edu>
2022-07-17 23:15:55 -0400

    update client services and pages

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-const landing = () => {
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-export default landing;
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentUser');
+  return data;
+};
+
+export default LandingPage;
