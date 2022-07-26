41c0aef938ea2b979622d17e8d3dd1c2cc2351b9
Chaojiaz <chaojiaz@andrew.cmu.edu>
2022-07-26 14:41:02 -0400

    update on Order with version added

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,21 @@
+
+const setup = async () => {
+  // Create an instance of the lisenter
+
+  // Create a fake data event
+
+  // Create a fake message object
+};
+
+it('creates and saves a ticket', async () => {
+  //  Call the onMessage function with the data object + message object
+
+  // Write asserstion to make sure a ticket was created
+});
+
+it('acks the message', async () => {
+
+  //  Call the onMessage function with the data object + message object
+
+  // Write assertion to make sure ack functio is called
+});
