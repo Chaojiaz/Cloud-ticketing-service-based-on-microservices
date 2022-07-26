408de6851e14e035c0ba8c8798f2a7961b9c36e6
Chaojiaz <chaojiaz@andrew.cmu.edu>
2022-07-25 23:28:11 -0400

    Event flow update

---
+++ orders/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@cjztickets/common';
+
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // allow cookie session to set cookie
+    // even the request is not secure (from https)
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+app.use(currentUser);
+
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError()
+});
+
+app.use(errorHandler);
+
+
+export { app };
