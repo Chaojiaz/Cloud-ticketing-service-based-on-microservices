0e6a6b94a886b985452036f695457e68657780a7
Chaojiaz <chaojiaz@andrew.cmu.edu>
2022-07-26 20:30:08 -0400

    ticket service with reserved ticket locked, and function checked by jest

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,6 +1,6 @@
 import express, { Response, Request } from 'express';
 import { body } from 'express-validator';
-import { validateRequest, NotFoundError, requireAuth, NotAuthorizedError } from '@cjztickets/common';
+import { validateRequest, NotFoundError, requireAuth, NotAuthorizedError, BadRequestError } from '@cjztickets/common';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
 import { natsWrapper } from '../nats-wrapper';
@@ -24,6 +24,10 @@ router.put('/api/tickets/:id', requireAuth,
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError('Cannot edit a reserved ticket');
     }
 
     if (ticket.userId != req.currentUser!.id) {
